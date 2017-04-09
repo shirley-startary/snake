6e9c7db1410ad43b1a4a8a9ee6354528bee3353e
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-09 01:13:59 -0500

    creando tabla dinamicamente

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,1 +1,27 @@
+var tamañoSnake=1;
+var ubicacion=[tamañoSnake-1];
+var botonCrearTabla=document.getElementById("botonCrearTabla");
+var lienzo=document.getElementById("lienzo");
+botonCrearTabla.addEventListener("click",crearTablaDinamica);
 
+function crearTablaDinamica(){
+  var fila=prompt("ingrese la cantidad de filas");
+  var columna=prompt("ingrese la cantidad de columnas");
+  var tabla=document.createElement("table");
+  for (var i=1;i<=fila;i++){
+    var crearFila=document.createElement("tr");
+    for(var j=1;j<=columna;j++){
+      var crearColumna=document.createElement("td");
+      crearColumna.id=i+"-"+j;
+      crearColumna.innerText=0;
+      crearColumna.style.background="yellow";
+      console.log(crearColumna);
+      crearFila.appendChild(crearColumna);
+      console.log(crearFila);
+    }
+    tabla.appendChild(crearFila);
+  }
+  lienzo.appendChild(tabla);
+  console.log(tabla);
+
+}
