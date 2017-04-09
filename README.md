7df0ed43fd4f94528e63e51b6b7a600ec526a547
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-09 01:55:01 -0500

    ubicando la víbora en su primer posicion

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 "Snake"
 1. creando Layout 5x5 con tabla.
+2. creando la tabla dinamicamente con ID Y Contenidos 0 como prueba.
+3. Ubicamos la víbora en su primer posicion, con la funcion ubicarSnike().
--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,5 +1,7 @@
 var tamañoSnake=1;
 var ubicacion=[tamañoSnake-1];
+ubicacion[0]="1-1";
+console.log(ubicacion);
 var botonCrearTabla=document.getElementById("botonCrearTabla");
 var lienzo=document.getElementById("lienzo");
 botonCrearTabla.addEventListener("click",crearTablaDinamica);
@@ -15,13 +17,25 @@ function crearTablaDinamica(){
       crearColumna.id=i+"-"+j;
       crearColumna.innerText=0;
       crearColumna.style.background="yellow";
-      console.log(crearColumna);
+      //console.log(crearColumna);
       crearFila.appendChild(crearColumna);
-      console.log(crearFila);
+      //console.log(crearFila);
     }
+
     tabla.appendChild(crearFila);
   }
+  tabla.style.cellspacing=0;
+  tabla.style.cellpadding=0;
   lienzo.appendChild(tabla);
   console.log(tabla);
+  ubicarSnike()
+
+}
+
+//en esta funcion ubicamos la víbora en su primer posicion
+function ubicarSnike(){
+  var iniciasnake=document.getElementById(ubicacion[0]);
+  iniciasnake.style.background="brown";
+  iniciasnake.innerText=1;
 
 }
