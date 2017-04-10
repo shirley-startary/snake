6c2bc871e700b5b14306cc752754efef0e25847d
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-10 01:05:32 -0500

    desplazando la vibora automaticamente

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 3. Ubicamos la víbora en su primer posicion, con la funcion ubicarSnike().
 4. Se crearon los movimientos con las funciones moverVibora(), desplazaVibora() ,
 5. Se creo la funcion terminarJuego().
+6. se crea la funcion autoMoverSnake() para que se mueva sola la vibora.
+7. tambien se creo la funcion renuevaUbicaciones(), para con la intencion de hacer crecer la vibora. "aun falta trabajar".
--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,9 +1,15 @@
+//Pide los valores del tamaño del recuadro
 var fila=parseInt(prompt("ingrese la cantidad de filas"));
 var columna=parseInt(prompt("ingrese la cantidad de columnas"));
+
+//Inicia los valores del juego
 var tamanoSnake=1;
-var ubicacion=[tamanoSnake-1];
-ubicacion[0]="2-1";
-console.log(ubicacion);
+var moverA="0,1";
+var ubicacion = new Array(tamanoSnake);
+var manzana = "2-2"
+ubicacion[tamanoSnake-1]="1-1";
+var tiempoEspera = 700;
+
 var botonCrearTabla=document.getElementById("botonCrearTabla");
 var lienzo=document.getElementById("lienzo");
 botonCrearTabla.addEventListener("click",function(){crearTablaDinamica(fila,columna)});
@@ -16,6 +22,10 @@ function crearTablaDinamica(){
       var crearColumna=document.createElement("td");
       crearColumna.style.width="10px";
       crearColumna.style.height="10px";
+      crearColumna.style.spacing="0";
+      crearColumna.style.padding="0";
+      crearColumna.style.border="0";
+      crearColumna.style.margin="0";
       crearColumna.id=i+"-"+j;
       //crearColumna.innerText=0;
       crearColumna.style.background="yellow";
@@ -26,95 +36,154 @@ function crearTablaDinamica(){
     }
     tabla.appendChild(crearFila);
   }
-  tabla.cellspacing="0";
-  tabla.cellpadding="0";
+  tabla.style.spacing="0";
+  tabla.style.padding="0";
+  tabla.style.margin="0";
+  tabla.style.border="0";
   lienzo.appendChild(tabla);
   console.log(tabla);
-  ubicarSnike()
+  ubicarSnike();
+
+  //dibujaPrimeraSnake();
   window.addEventListener("keydown",moverVibora);
+  window.setTimeout("autoMoverSnake()",tiempoEspera);
 }
 
 //en esta funcion ubicamos la víbora en su primer posicion
 function ubicarSnike(){
+
+  validaBordes();
   var iniciasnake=document.getElementById(ubicacion[tamanoSnake-1]);
   iniciasnake.style.background="brown";
   //iniciasnake.innerText=1;
-
 }
 
 function desplazaVibora(cordenada){
 
-  var cola=document.getElementById(ubicacion[0]);
+  var cola=document.getElementById(ubicacion[tamanoSnake-1]);
   //cola.innerText=0;
   cola.style.background="yellow";
-  ubicacion[0]=cordenada;
+  ubicacion[tamanoSnake-1]=cordenada;
   ubicarSnike();
 }
-
 
 function moverVibora(event){
   var leerCabeza=ubicacion[tamanoSnake-1]
   var cordenadas = leerCabeza.split("-");
-  console.log(cordenadas);
-    switch(event.key){
-        case "ArrowUp":
-            cordenadas[0]--;
-            //console.log(cordenadas);
-            var nuevaCordenada = cordenadas.join("-");
-            //console.log(nuevaCordenada);
+  //console.log(cordenadas);
+  switch(event.key){
+      case "ArrowUp":
+          moverA="-1,0";
+          cordenadas[0]--;
+          //console.log(cordenadas);
+          var nuevaCordenada = cordenadas.join("-");
+          //console.log(nuevaCordenada);
 
-            if(cordenadas[0] == 0){
-              terminarJuego();
-            }else {
-              desplazaVibora(nuevaCordenada);
-            }
-
-            break;
-        case "ArrowDown":
-            cordenadas[0]++;
-            //console.log(cordenadas);
-            var nuevaCordenada = cordenadas.join("-");
-            //console.log(nuevaCordenada);
-
-            if(cordenadas[0] == 1+fila){
-              terminarJuego();
-            }else {
-              desplazaVibora(nuevaCordenada);
-            }
-            break;
-        case "ArrowLeft":
-            cordenadas[1]--;
-            //console.log(cordenadas);
-            var nuevaCordenada = cordenadas.join("-");
-            //console.log(nuevaCordenada);
-
-            if(cordenadas[1] == 0){
-              terminarJuego();
-            }else{
-              desplazaVibora(nuevaCordenada);
-            }
-            break;
-        case "ArrowRight":
-            cordenadas[1]++;
-            //console.log(cordenadas);
-            var nuevaCordenada = cordenadas.join("-");
-            //console.log(nuevaCordenada);
-            console.log(1+columna);
-            console.log(columna+1);
+          if(cordenadas[0] != 0){
+            desplazaVibora(nuevaCordenada);
+          }
 
 
-            if(cordenadas[1] == 1+columna){
-              terminarJuego();
-            }else {
-              desplazaVibora(nuevaCordenada);
-            }
-            break;
+          break;
+      case "ArrowDown":
+          moverA="1,0";
+          cordenadas[0]++;
+          //console.log(cordenadas);
+          var nuevaCordenada = cordenadas.join("-");
+          //console.log(nuevaCordenada);
 
+          if(cordenadas[0] != 1+fila){
+            desplazaVibora(nuevaCordenada);
+          }
+          break;
 
+      case "ArrowLeft":
+          moverA="0,-1";
+          cordenadas[1]--;
+          //console.log(cordenadas);
+          var nuevaCordenada = cordenadas.join("-");
+          //console.log(nuevaCordenada);
+
+          if(cordenadas[1] !== 0){
+            desplazaVibora(nuevaCordenada);
+          }
+          break;
+
+      case "ArrowRight":
+          moverA="0,1";
+          cordenadas[1]++;
+          //console.log(cordenadas);
+          var nuevaCordenada = cordenadas.join("-");
+          //console.log(nuevaCordenada);
+
+          if(cordenadas[1] != 1+columna){
+            desplazaVibora(nuevaCordenada);
+
+          break;
     }
+  }
 }
 
+
 function terminarJuego(){
-    alert("***GAME OVER***");
+    alert("**GAME OVER**");
     window.removeEventListener("keydown",moverVibora);
+    window.clearTimeout();
+}
+
+function autoMoverSnake(){
+  var desplazamiento = moverA.split(",");
+  var ubicacionCabeza = ubicacion[tamanoSnake-1].split("-");
+
+  var nuevaCordenada = (parseInt(ubicacionCabeza[0]) + parseInt(desplazamiento[0])) + "-" + (parseInt(ubicacionCabeza[1]) + parseInt(desplazamiento[1]));
+
+  renuevaUbicaciones(nuevaCordenada);
+  ubicarSnike();
+  window.setTimeout("autoMoverSnake()",tiempoEspera);
+}
+
+
+function renuevaUbicaciones(nuevaCordenada){
+  //
+  if(tamanoSnake!=1){
+    for(i==0;i<=tamanoSnake-1;i++){
+      ubicacion[i] = ubicacion[i+1];
+    }
+  }
+  desplazaVibora(nuevaCordenada);
+}
+
+
+function validaBordes(){
+  //valida los que no se salga la vibora de la tabla, en caso de sobre pasar
+  //llama a la funcion terminarJuego() el cual desplega un mensaje de Game over
+  var leerCabeza=ubicacion[tamanoSnake-1]
+  var cordenadas = leerCabeza.split("-");
+  //console.log(cordenadas);
+
+      switch(moverA){
+        case "-1,0":
+            if(parseInt(cordenadas[0]) <= 0){
+              terminarJuego();
+            }
+            break;
+
+        case "1,0":
+            if(parseInt(cordenadas[0]) >= 1+fila){
+              terminarJuego();
+            }
+            break;
+
+        case "0,-1":
+            if(parseInt(cordenadas[1]) <= 0){
+              terminarJuego();
+            }
+            break;
+
+        case "0,1":
+            if(parseInt(cordenadas[1]) >= 1+columna){
+              terminarJuego();
+            }
+            break;
+    }
 }
